1213771405aaa2902552ed7aa7be14c40667e101
ryankuah <ryk326@lehigh.edu>
2023-05-29 17:25:10 -0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Box, Button, ChakraProvider, Flex, Heading, Image, Text} from "@chakra-ui/react";
+
+function App() {git remote add origin https://github.com/ryankuah/website.git
+  return (
+    <Text>
+      Hello World
+    </Text>
+  );
+}
+
+export default App;
