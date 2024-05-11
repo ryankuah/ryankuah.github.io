3b7d465da2a1d8e4d0406763f52f5038522e4e59
ryankuah <ryk326@lehigh.edu>
2024-05-11 21:10:14 +1000

    attempt to fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Box, Button, ChakraProvider, Flex, Heading, Image, Switch, Text} from "@chakra-ui/react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, HashRouter} from "react-router-dom";
 
 import {HomePage, AboutPage, ProjectPage, SomethingElse} from "./pages"
 import Theme from "./Theme";
@@ -8,14 +8,14 @@ import Theme from "./Theme";
 function App() {
   return (
     <ChakraProvider theme={Theme}>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/about" element={<AboutPage/>}/>
           <Route path="/projects" element={<ProjectPage/>}/>
           <Route path="/somethingelse" element={<SomethingElse/>}/>
         </Routes>
-      </Router>
+      </HashRouter>
     </ChakraProvider>
   );
 }
