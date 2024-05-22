65111aa993797df1349760f5c1c5d69dc8e756ed
ryankuah <ryk326@lehigh.edu>
2024-05-22 17:41:42 +1000

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {Box, Button, ChakraProvider, Flex, Heading, Image, Switch, Text} from "@chakra-ui/react";
-import {BrowserRouter as Router, Route, Routes, HashRouter, BrowserRouter} from "react-router-dom";
+import { Box, Button, ChakraProvider, Flex, Heading, Image, Switch, Text } from "@chakra-ui/react";
+import { BrowserRouter as Router, Route, Routes, HashRouter, BrowserRouter } from "react-router-dom";
 
-import {HomePage, AboutPage, ProjectPage, SomethingElse} from "./pages"
+import { HomePage, AboutPage, ProjectPage, SomethingElse, ComingSoonPage } from "./pages"
+
 import Theme from "./Theme";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/about" element={<AboutPage/>}/>
-        <Route path="/projects" element={<ProjectPage/>}/>
-        <Route path="/somethingelse" element={<SomethingElse/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectPage />} />
+        <Route path="/somethingelse" element={<SomethingElse />} />
       </Routes>
     </HashRouter>
   );
